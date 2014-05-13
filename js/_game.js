8617c20cf8aa1a34ce9610f0f7641ae09dde4f13
Job van Kaathoven <info@jobvankaathoven.nl>
2014-05-13 13:31:40 +0200

    test comment

--- js/_game.js
+++ js/_game.js
@@ -5,6 +5,8 @@ var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '
     render: render
 });
 
+//test
+
 function preload() {
 	game.load.spritesheet('player', 'img/ally_sprite.png', 64, 64);
 	game.load.image('boss', 'img/boss.png');
